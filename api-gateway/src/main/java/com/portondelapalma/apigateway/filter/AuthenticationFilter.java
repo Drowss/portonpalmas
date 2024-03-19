0afe5fd773ac7cc961b0607b4b747fb34847cbf8
Drow <esneiderhel@gmail.com>
2024-03-19 14:04:02 -0500

    permisos-working

--- api-gateway/src/main/java/com/portondelapalma/apigateway/filter/AuthenticationFilter.java
+++ api-gateway/src/main/java/com/portondelapalma/apigateway/filter/AuthenticationFilter.java
@@ -25,7 +25,9 @@ import reactor.core.publisher.Mono;
 
 import javax.crypto.SecretKey;
 import java.time.Instant;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 @RefreshScope
 @Component
@@ -47,29 +49,54 @@ public class AuthenticationFilter implements GatewayFilter {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        System.out.println("HOLAAAAAAAAA");
         ServerHttpRequest request = exchange.getRequest();
         ServerHttpResponse response = exchange.getResponse();
 
-        if (this.isSecured(request)) {
-            if (!request.getCookies().containsKey("token")) {
-                System.out.println("KEY NOT FOUND");
+        if (this.isAdmin(request)) {
+            try {
+                HttpCookie cookie = request.getCookies().get("token").get(0);
+                String accessToken = cookie.getValue();
+                if (this.isExpired(accessToken)) {
+                    System.out.println("EXPIRED");
+                    response.setStatusCode(HttpStatus.UNAUTHORIZED);
+
+                    return response.setComplete();
+                }
+                Claims accessClaims = Jwts.parser()
+                        .verifyWith((key))
+                        .build()
+                        .parseSignedClaims(accessToken)
+                        .getPayload();
+
+                String role = accessClaims.get("role", String.class);
+
+                if (!"ADMIN".equals(role)) {
+                    response.setStatusCode(HttpStatus.UNAUTHORIZED);
+                    return response.setComplete();
+                }
+            } catch (Exception ex) {
+                System.out.println("EXCEPTION");
+                ex.printStackTrace();
                 response.setStatusCode(HttpStatus.UNAUTHORIZED);
                 return response.setComplete();
             }
-
-            HttpCookie cookie = request.getCookies().get("token").get(0);
-            String accessToken = cookie.getValue();
-
-            if (this.isExpired(accessToken)) {
-                System.out.println("EXPIRED");
-                response.setStatusCode(HttpStatus.UNAUTHORIZED);
-
-                return response.setComplete();
-            }
-            System.out.println("PASSED");
         }
+        System.out.println("PASSED");
+
         return chain.filter(exchange);
+    }
+
+    private boolean isAdmin(ServerHttpRequest request) {
+        String path = request.getURI().getPath();
+        List<String> securedRoutes = Arrays.asList(
+                "/product/v1/upload",
+                "/product/v1/delete",
+                "/product/v1/put",
+                "/horse/v1/upload",
+                "/horse/v1/delete",
+                "/horse/v1/put"
+        );
+        return securedRoutes.stream().anyMatch(path::startsWith);
     }
 
     private boolean isSecured(ServerHttpRequest request) {
